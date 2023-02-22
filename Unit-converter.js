8ac838dc9f5a203719f7c436d3d14708c0b49a77
bamidele emmanuel <emmanuelbam9876@gmail.com>
2023-02-22 09:54:48 -0800

    first commit

---
+++ Unit-converter.js
@@ -0,0 +1,21 @@
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("inpute-el")
+let lengthInput = document.getElementById("length-input")
+let volumeInput = document.getElementById("volume-input")
+let massInput = document.getElementById("mass-input")
+
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+
+convertBtn.addEventListener("click", function() {
+    let baseValue = inputEl.value.
+
+    lengthEl.textcontent = `${baseValue} meter = ${baseValue * meterToFeet} feet`;
+
+    volumeInput.textContent = `${baseValue} Liters = ${baseValue * literToGallon} Gallons`;
+
+    massInput.textContent = `${baseValue} Kilograms = ${kiloToPound} pound`;
+    
+})
